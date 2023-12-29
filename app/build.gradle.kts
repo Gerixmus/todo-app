891b715c5aab41d54bf9c420d3819df201e2ebe4
Gerixmus <gerixmuss@gmail.com>
2023-12-29 19:43:21 +0100

    test: Added tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 }
 
 dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("junit:junit:4.12")
     val room_version = "2.6.1"
     val activity_version = "1.8.1"
     val fragment_version = "1.6.2"
@@ -94,4 +96,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test:rules:1.4.0")
 }
--- app/src/androidTest/java/com/example/todolist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/todolist/ExampleInstrumentedTest.kt
@@ -1,5 +1,10 @@
 package com.example.todolist
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +12,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,41 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.todolist", appContext.packageName)
     }
+
+    @Test
+    fun testNewTaskButton() {
+        // Click on the new task button
+        Espresso.onView(ViewMatchers.withId(R.id.newTaskButton)).perform(ViewActions.click())
+        // Check if the dialog is displayed
+        Espresso.onView(ViewMatchers.withText("New Task")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun testRecyclerView() {
+        // Check if the RecyclerView is displayed
+        Espresso.onView(ViewMatchers.withId(R.id.todoListRecycleView)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun testEditTaskItem() {
+        // Create a sample TaskItem for testing
+        val sampleTaskItem = TaskItem(name = "sample", desc = "Sample Task", completedDateString = "", dueTimeString = "12:30")
+
+        // Call editTaskItem with the sample TaskItem
+        activityScenarioRule.scenario.onActivity { activity ->
+            activity.editTaskItem(sampleTaskItem)
+        }
+
+        // Verify that the NewTaskSheet dialog is displayed
+        Espresso.onView(ViewMatchers.withText("Edit Task")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
 }
